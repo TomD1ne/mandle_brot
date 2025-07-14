44d3e700d21e829446a6a7063904e76aca9b6414
Tom Diterwich <t.c.diterwich@students.uu.nl>
2025-07-15 00:41:07 +0200

    feat: working

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use async_std::task::{self, JoinHandle};
-use futures::future::join_all;
+use futures::{future::join_all, lock::Mutex};
 use num::complex::Complex;
 use speedy2d::{
     color::Color,
@@ -8,17 +8,22 @@ use speedy2d::{
     Graphics2D, Window,
 };
 use std::{
+    collections::VecDeque,
     env,
     future::{ready, IntoFuture, Ready},
-    thread,
-    time::Instant,
+    ops::Add,
+    sync::{
+        atomic::{AtomicU8, AtomicUsize},
+        Arc,
+    },
+    thread::{self, sleep},
+    time::{self, Duration, Instant},
 };
 
 const WINDOW_WIDTH: u32 = 2048;
 const WINDOW_HEIGHT: u32 = 2048;
-const MAX_DEPTH: u32 = 1000;
-// const MAX_COLORS: u32 = 16000000;
-// const COLOR_FACTOR: u32 = MAX_COLORS / MAX_DEPTH;
+const MAX_DEPTH: u32 = 10000;
+const THREAD_COUNT: usize = 32;
 
 #[derive(Copy, Clone)]
 struct Zoom {
@@ -45,6 +50,7 @@ impl Zoom {
     }
 }
 
+#[derive(Clone)]
 struct Bitmap {
     pixels: Vec<u8>,
     size: (u32, u32),
@@ -60,6 +66,14 @@ impl IntoFuture for Bitmap {
     }
 }
 
+struct WorkItem {
+    br_x: u32,
+    br_y: u32,
+    tl_x: u32,
+    tl_y: u32,
+    must_split: bool,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let timings = args.iter().any(|a| a == "timing");
@@ -68,11 +82,6 @@ fn main() {
     let zoom_status = Zoom::new(1.0, 0.0, 0.0);
     let mouse_position: Vector2<f64> = Vector2::new(0.0, 0.0);
     let bitmaps: Vec<Bitmap> = vec![];
-
-    // if timings {
-    //     println!("{} {}", timings, &args[1]);
-    //     thread::spawn(|| test(&window));
-    // }
 
     window.run_loop(MyWindowHandler {
         zoom_status,
@@ -93,7 +102,7 @@ fn test(window_handler: &mut MyWindowHandler, helper: &WindowHelper) {
 
         futures::executor::block_on(async {
             let split_spawn_time = Instant::now();
-            window_handler.bitmaps = split_and_spawn(8, 8, z).await;
+            window_handler.bitmaps = split_and_spawn(z).await;
             helper.request_redraw();
 
             let t = split_spawn_time.elapsed().as_millis() as f32 / 1000.0;
@@ -151,6 +160,11 @@ fn get_pixel_color(x: u32, y: u32, zoom: &Zoom) -> Color {
 
 fn set_pixel_color(x: u32, y: u32, color: Color, bitmap: &mut Bitmap) {
     let pixel = (x - bitmap.location.0 + bitmap.size.0 * (y - bitmap.location.1)) * 4;
+    // println!(
+    //     "{} - {} + {} * ({} - {})",
+    //     x, bitmap.location.0, bitmap.size.0, y, bitmap.location.1
+    // );
+    // println!("{}", pixel);
     bitmap.pixels[pixel as usize] = (color.r() * 255.0) as u8;
     bitmap.pixels[pixel as usize + 1] = (color.g() * 255.0) as u8;
     bitmap.pixels[pixel as usize + 2] = (color.b() * 255.0) as u8;
@@ -158,12 +172,15 @@ fn set_pixel_color(x: u32, y: u32, color: Color, bitmap: &mut Bitmap) {
 }
 
 fn calculate_rectangle(
-    tl_x: u32,
-    tl_y: u32,
-    br_x: u32,
-    br_y: u32,
-    bitmap: &mut Bitmap,
+    WorkItem {
+        br_x,
+        br_y,
+        tl_x,
+        tl_y,
+        ..
+    }: WorkItem,
     zoom: &Zoom,
+    bitmap: &mut Bitmap,
 ) {
     for x in tl_x..=br_x {
         for y in tl_y..=br_y {
@@ -173,99 +190,157 @@ fn calculate_rectangle(
     }
 }
 
-async fn split_and_spawn(n_x: u32, n_y: u32, zoom: Zoom) -> Vec<Bitmap> {
-    let width = WINDOW_WIDTH / n_x;
-    let height = WINDOW_HEIGHT / n_y;
-    let mut tasks: Vec<JoinHandle<_>> = vec![];
-    for x in 0..n_x {
-        for y in 0..n_y {
-            tasks.push(task::spawn(async move {
-                let mut bitmap = Bitmap {
-                    pixels: vec![0; (width * height * 4) as usize],
-                    size: (width, height),
-                    location: (x * width, y * height),
-                };
-                split(
-                    bitmap.location.0,
-                    bitmap.location.1,
-                    bitmap.location.0 + bitmap.size.0 - 1,
-                    bitmap.location.1 + bitmap.size.1 - 1,
-                    0,
-                    &mut bitmap,
-                    &zoom.clone(),
-                );
-                bitmap
-            }));
-        }
+async fn split_and_spawn(zoom: Zoom) -> Vec<Bitmap> {
+    let mut threads: Vec<JoinHandle<_>> = Vec::with_capacity(THREAD_COUNT);
+    let bitmaps: Arc<Mutex<Vec<Bitmap>>> = Arc::new(Mutex::new(vec![]));
+    // let busy_count: Arc<AtomicUsize> = Arc::new(AtomicUsize::new(0));
+    let busy_count: Arc<Mutex<u8>> = Arc::new(Mutex::new(0));
+    let deque: Arc<Mutex<VecDeque<WorkItem>>> = Arc::new(Mutex::new(VecDeque::new()));
+    deque.lock().await.push_back(WorkItem {
+        br_x: WINDOW_HEIGHT - 1,
+        br_y: WINDOW_WIDTH - 1,
+        tl_x: 0,
+        tl_y: 0,
+        must_split: true,
+    });
+
+    for _ in 0..THREAD_COUNT {
+        threads.push(task::spawn(work(
+            Arc::clone(&bitmaps),
+            zoom.clone(),
+            Arc::clone(&busy_count),
+            Arc::clone(&deque),
+        )))
     }
-    join_all(tasks).await
+
+    join_all(threads).await;
+    let bitmap = Arc::clone(&bitmaps).lock().await.clone();
+    bitmap
 }
 
-fn split(tl_x: u32, tl_y: u32, br_x: u32, br_y: u32, level: u8, bitmap: &mut Bitmap, zoom: &Zoom) {
-    let width = br_x - tl_x;
-    let height = br_y - tl_y;
-    let mut must_split = false;
+async fn work(
+    bitmaps: Arc<Mutex<Vec<Bitmap>>>,
+    zoom: Zoom,
+    busy_count: Arc<Mutex<u8>>,
+    deque: Arc<Mutex<VecDeque<WorkItem>>>,
+) {
+    let mut keep_going = true;
+    while keep_going {
+        let work_item = deque.lock().await.pop_front();
+        match work_item {
+            None => {
+                // println!("STOP {}", count);
+                if busy_count.lock().await.eq(&0) {
+                    keep_going = false;
+                } else {
+                    thread::yield_now();
+                }
+            }
+            Some(item) => {
+                {
+                    let mut lock = busy_count.lock().await;
+                    *lock += 1;
+                }
+                // println!(
+                //     "Thread {:?} incremented busy_count to {}",
+                //     std::thread::current().id(),
+                //     busy_count.load(std::sync::atomic::Ordering::SeqCst)
+                // );
+                let WorkItem {
+                    br_x,
+                    br_y,
+                    tl_x,
+                    tl_y,
+                    must_split,
+                } = item;
+                let width = br_x - tl_x + 1;
+                let height = br_y - tl_y + 1;
+                let mut must_split = must_split;
+                let mut bitmap: Bitmap = Bitmap {
+                    pixels: vec![0; (width * height * 4) as usize],
+                    size: (width, height),
+                    location: (tl_x, tl_y),
+                };
+                if width <= 8 || height <= 8 {
+                    calculate_rectangle(item, &zoom, &mut bitmap);
+                } else {
+                    for x in tl_x..=br_x {
+                        let top_color: Color = get_pixel_color(x, tl_y, &zoom);
+                        let bottom_color: Color = get_pixel_color(x, br_y, &zoom);
+                        set_pixel_color(x, tl_y, top_color, &mut bitmap);
+                        set_pixel_color(x, br_y, bottom_color, &mut bitmap);
 
-    if width <= 17 || height <= 17 {
-        calculate_rectangle(tl_x, tl_y, br_x, br_y, bitmap, zoom);
-        return;
-    }
+                        if top_color != Color::BLACK || bottom_color != Color::BLACK {
+                            must_split = true;
+                        }
+                    }
 
-    for x in tl_x..=br_x {
-        let top_color: Color = get_pixel_color(x, tl_y, zoom);
-        let bottom_color: Color = get_pixel_color(x, br_y, zoom);
-        set_pixel_color(x, tl_y, top_color, bitmap);
-        set_pixel_color(x, br_y, bottom_color, bitmap);
+                    for y in tl_y + 1..=br_y - 1 {
+                        let left_color: Color = get_pixel_color(tl_x, y, &zoom);
+                        let right_color: Color = get_pixel_color(br_x, y, &zoom);
+                        set_pixel_color(tl_x, y, left_color, &mut bitmap);
+                        set_pixel_color(br_x, y, right_color, &mut bitmap);
 
-        if top_color != Color::BLACK || bottom_color != Color::BLACK {
-            must_split = true;
+                        if left_color != Color::BLACK || right_color != Color::BLACK {
+                            must_split = true;
+                        }
+                    }
+
+                    if must_split {
+                        let mid_x = tl_x + width / 2;
+                        let mid_y = tl_y + height / 2;
+
+                        let tl_split = WorkItem {
+                            tl_x: tl_x + 1,
+                            tl_y: tl_y + 1,
+                            br_x: mid_x - 1,
+                            br_y: mid_y - 1,
+                            must_split: false,
+                        };
+
+                        let tr_split = WorkItem {
+                            tl_x: mid_x,
+                            tl_y: tl_y + 1,
+                            br_x: br_x - 1,
+                            br_y: mid_y - 1,
+                            must_split: false,
+                        };
+
+                        let bl_split = WorkItem {
+                            tl_x: tl_x + 1,
+                            tl_y: mid_y,
+                            br_x: mid_x - 1,
+                            br_y: br_y - 1,
+                            must_split: false,
+                        };
+
+                        let br_split = WorkItem {
+                            tl_x: mid_x,
+                            tl_y: mid_y,
+                            br_x: br_x - 1,
+                            br_y: br_y - 1,
+                            must_split: false,
+                        };
+                        {
+                            let mut lock = deque.lock().await;
+                            lock.push_back(tl_split);
+                            lock.push_back(tr_split);
+                            lock.push_back(bl_split);
+                            lock.push_back(br_split);
+                        }
+                    }
+                }
+                bitmaps.lock().await.push(bitmap);
+                {
+                    let mut lock = busy_count.lock().await;
+                    *lock -= 1;
+                }
+                // println!(
+                //     "Thread {:?} about to decrement busy_count",
+                //     std::thread::current().id()
+                // );
+            }
         }
-    }
-    for y in tl_y + 1..=br_y - 1 {
-        let left_color: Color = get_pixel_color(tl_x, y, zoom);
-        let right_color: Color = get_pixel_color(br_x, y, zoom);
-        set_pixel_color(tl_x, y, left_color, bitmap);
-        set_pixel_color(br_x, y, right_color, bitmap);
-
-        if left_color != Color::BLACK || right_color != Color::BLACK {
-            must_split = true;
-        }
-    }
-    if level == 0 {
-        must_split = true;
-    }
-    if must_split {
-        let mid_x = tl_x + width / 2;
-        let mid_y = tl_y + height / 2;
-
-        split(
-            tl_x + 1,
-            tl_y + 1,
-            mid_x - 1,
-            mid_y - 1,
-            level + 1,
-            bitmap,
-            zoom,
-        );
-        split(
-            mid_x,
-            tl_y + 1,
-            br_x - 1,
-            mid_y - 1,
-            level + 1,
-            bitmap,
-            zoom,
-        );
-        split(
-            tl_x + 1,
-            mid_y,
-            mid_x - 1,
-            br_y - 1,
-            level + 1,
-            bitmap,
-            zoom,
-        );
-        split(mid_x, mid_y, br_x - 1, br_y - 1, level + 1, bitmap, zoom);
     }
 }
 
@@ -291,7 +366,7 @@ impl WindowHandler for MyWindowHandler {
         }
 
         futures::executor::block_on(async {
-            self.bitmaps = split_and_spawn(4, 4, self.zoom_status).await;
+            self.bitmaps = split_and_spawn(self.zoom_status).await;
             helper.request_redraw();
             // println!(
             //     "{}x loc({}, {}) t: {}",
@@ -312,7 +387,7 @@ impl WindowHandler for MyWindowHandler {
         for bitmap in &self.bitmaps {
             let result = graphics.create_image_from_raw_pixels(
                 speedy2d::image::ImageDataType::RGBA,
-                speedy2d::image::ImageSmoothingMode::NearestNeighbor,
+                speedy2d::image::ImageSmoothingMode::Linear,
                 bitmap.size,
                 &bitmap.pixels,
             );
@@ -352,15 +427,8 @@ impl WindowHandler for MyWindowHandler {
         }
 
         futures::executor::block_on(async {
-            let now = Instant::now();
-            self.bitmaps = split_and_spawn(8, 8, self.zoom_status).await;
+            self.bitmaps = split_and_spawn(self.zoom_status).await;
             helper.request_redraw();
-
-            // let t = now.elapsed().as_millis() as f32 / 1000.0;
-            // println!(
-            //     "{}x loc({}, {}) t: {}",
-            //     self.zoom_status.zoom_factor, self.zoom_status.term_x, self.zoom_status.term_y, t
-            // );
         });
     }
 
